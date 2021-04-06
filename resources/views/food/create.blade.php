22ee079e9be09be1928f8dea4182fef56a291456
Nslkh <olluzran@gmail.com>
2021-04-06 23:47:20 +0500

    validation of the form

--- resources/views/food/create.blade.php
+++ resources/views/food/create.blade.php
@@ -2,47 +2,74 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-              <div class="card-header">{{ __('Add new food') }}</div>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <form action="{{ route('food.store') }}" method="POST">@csrf
+        <div class="card">
+          <div class="card-header">{{ __('Add new food') }}</div>
 
-              <div class="card-body">
-                <div class="form-group">
-                  <label for="name">Name</label>
-                  <input type="text" name="name" class="form-control">
-                </div>
+          <div class="card-body">
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" >
+              @error('name')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+            </div>
 
-                <div class="form-group">
-                  <label for="description">Description</label>
-                  <input type="text" name="description" class="form-control">
-                </div>
+            <div class="form-group">
+              <label for="description">Description</label>
+              <textarea  name="description" class="form-control @error('description') is-invalid @enderror"></textarea>
+              @error('description')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+            </div>
 
-                <div class="form-group">
-                  <label for="price">Price</label>
-                  <input type="number" name="name" class="form-control">
-                </div>
+            <div class="form-group">
+              <label for="price">Price</label>
+              <input type="number" name="name" class="form-control @error('price') is-invalid @enderror" >
+              @error('price')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+            </div>
 
-                <div class="form-group">
-                  <label for="name">Category</label>
-                  <select name="category" class="form-control">
-                    <option value="">Select Category</option>
-                    @foreach (App\Models\Category::all() as $category)
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                    @endforeach
-                  </select>
-                </div>
+            <div class="form-group">
+              <label for="name">Category</label>
+              <select name="category" class="form-control @error('category') is-invalid @enderror" >
+                <option value="">Select Category</option>
+                @foreach (App\Models\Category::all() as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
+              </select>
+              @error('category')
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span>
+              @enderror
+            </div>
 
-                <div class="form-group">
-                  <label for="image">Image</label>
-                  <input type="text" name="image" class="form-control">
-                </div>
-                
-                <div class="form-group">
-                  <button class="btn btn-outline-primary" type="submit">Submit</button>
-                </div>
+            <div class="form-group">
+              <label for="image">Image</label>
+              <input type="text" name="image" class="form-control @error('image') is-invalid @enderror" >
+              @error('image')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+            </div>
+            
+            <div class="form-group">
+              <button class="btn btn-outline-primary" type="submit">Submit</button>
             </div>
         </div>
+      </form>  
     </div>
+  </div>
 </div>
 @endsection
