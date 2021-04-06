5daf143423d862e981e8c87ba14e180c9907a108
Nslkh <olluzran@gmail.com>
2021-04-06 14:58:02 +0500

    controller and category form

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -23,7 +23,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('category.create');
     }
 
     /**
@@ -34,7 +34,7 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return "ok";
     }
 
     /**
